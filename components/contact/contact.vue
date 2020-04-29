b173e6be83ab4cd3054f9a9a4a740155c8241296
乘风破浪- <1441574114@qq.com>
2020-04-29 18:36:24 +0800

    如意通卡

---
+++ components/contact/contact.vue
@@ -0,0 +1,35 @@
+<template>
+	<view class="contact">
+		<button type="default" class="contact-btn" open-type="contact">
+			<text class="iconfont icon-xiaomi"></text>
+			<view>客服</view>
+		</button>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style scoped lang="less">
+	.contact {
+		.contact-btn {
+			position: fixed;
+			bottom: 20%;
+			right: 24rpx;
+			z-index: 999999;
+			width: 88rpx;
+			height: 88rpx;
+			line-height: 32rpx;
+			font-size: 24rpx;
+			color: #FFFFFF;
+			padding: 0;
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			background-color: rgba(0,0,0,0.6)!important;
+		}
+		.contact-btn {
+			border: none;
+		}
+	}
+</style>
